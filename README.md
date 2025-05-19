fdd4d7c530f3177783a0c30dc9c00e40ddaad102
lou1s <lou1svuong.dev@gmail.com>
2025-05-20 01:17:48 +0700

    commit 0 of 2025-05-20 with count 1

--- README.md
+++ README.md
@@ -35,3 +35,4 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
 # lou1s-sui-2025
+- Added fake commit commit 0 of 2025-05-20 with count 1 with 1 commits
--- components/providers/wallet-provider.tsx
+++ components/providers/wallet-provider.tsx
@@ -10,21 +10,39 @@ import {
   SuiTestnetChain,
   WalletProvider,
 } from "@suiet/wallet-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
+import { createNetworkConfig, SuiClientProvider } from "@mysten/dapp-kit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 export function WalletProviders({ children }: { children: any }) {
   const SupportedChains: Chain[] = [
     SuiDevnetChain,
     SuiTestnetChain,
     SuiMainnetChain,
   ];
+
+  const { networkConfig } = createNetworkConfig({
+    testnet: { url: getFullnodeUrl("testnet") },
+    localnet: { url: getFullnodeUrl("localnet") },
+    mainnet: { url: getFullnodeUrl("mainnet") },
+  });
+
   // const [mounted, setMounted] = React.useState(false);
 
   // React.useEffect(() => {
   //   setMounted(true);
   // }, []);
+  const queryClient = new QueryClient();
 
   return (
-    <WalletProvider autoConnect chains={SupportedChains}>
-      {children}
-    </WalletProvider>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+        <WalletProvider>
+          <WalletProvider autoConnect chains={SupportedChains}>
+            {children}
+          </WalletProvider>
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   );
 }
