ad2be8bbf86479cafc579ae5bb315f5711532cdf
lou1s <lou1svuong.dev@gmail.com>
2025-06-04 20:51:26 +0700

    Initial commit

---
+++ config/site.config.tsx
@@ -0,0 +1,60 @@
+import Github from "@/components/icons/github";
+import Mail from "@/components/icons/mail";
+import Telegram from "@/components/icons/telegram";
+import X from "@/components/icons/x";
+import { SiteConfig } from "@/types";
+import { Linkedin } from "lucide-react";
+
+export const siteConfig: SiteConfig = {
+  name: "Lou1s",
+  title: "Lou1s - Software Engineer",
+  description:
+    "A fullstack developer with a growing interest in Web3 technologies. I'm exploring decentralized applications, blockchain, and the next generation of the internet. Through continuous learning and hands-on projects, I'm excited to contribute to the evolving world of decentralized finance (DeFi), smart contracts, and tokenized assets.",
+  origin: "https://lou1sgudboiz.xyz",
+  keywords: [
+    "Lou1s Portfolio",
+    "Lou1s Dev",
+    "Lou1s Web3",
+    "Lou1s Blockchain",
+    "Lou1s Smart Contract",
+    "Lou1s Fullstack",
+    "Lou1s Frontend",
+    "Lou1s Backend",
+    "Lou1s Web3 Developer",
+    "Lou1s Web3 Developer",
+    "Mai Vu Xuan Vuong",
+    "Mai Vu Xuan Vuong Portfolio",
+    "Mai Vu Xuan Vuong Dev",
+    "Mai Vu Xuan Vuong Web3",
+    "Mai Vu Xuan Vuong Blockchain",
+    "Mai Vu Xuan Vuong Smart Contract",
+    "Mai Vu Xuan Vuong Fullstack",
+  ],
+  og: "https://lou1sgudboiz.xyz/og.png",
+  creator: {
+    name: "Lou1s",
+    url: "https://lou1sgudboiz.xyz",
+  },
+  socials: {
+    // email: {
+    //   href: "mailto:lou1svuong.dev@gmail.com",
+    //   icon: Mail,
+    // },
+    // linkedin: {
+    //   href: "https://www.linkedin.com/in/xuanvuong/",
+    //   icon: Linkedin,
+    // },
+    github: {
+      href: "https://github.com/Lou1sVuong",
+      icon: Github,
+    },
+    x: {
+      href: "https://x.com/lou1sgudboiz",
+      icon: X,
+    },
+    telegram: {
+      href: "https://t.me/lou1sgudboiz",
+      icon: Telegram,
+    },
+  },
+};
