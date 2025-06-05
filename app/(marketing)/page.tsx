a40fa75ab8b66f0c3b49c0ec436c3a5614005347
lou1s <lou1svuong.dev@gmail.com>
2025-06-05 12:08:57 +0700

    Remove unused Footer import and commented-out AnnouncementBar from Home page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,9 @@
-import Footer from "@/components/layouts/footer";
 import Header from "@/components/layouts/header";
 
 export default function Home() {
   return (
     <div className="w-full md:overflow-hidden flex flex-col items-center justify-center">
       <div className=" mx-auto border border-dashed flex flex-col my-40 items-center">
-        {/* <AnnouncementBar /> */}
         <Header />
       </div>
     </div>
