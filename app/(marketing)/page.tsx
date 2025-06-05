c752d0b99a4456d180aca7c8a674de3a6300f230
lou1s <lou1svuong.dev@gmail.com>
2025-06-05 12:36:06 +0700

    Add SignAndVerify component to Home page for enhanced functionality

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
+import SignAndVerify from "@/components/examples/sign-and-verify";
 import Header from "@/components/layouts/header";
 
 export default function Home() {
   return (
     <div className="w-full md:overflow-hidden flex flex-col items-center justify-center">
-      <div className=" mx-auto border border-dashed flex flex-col my-40 items-center">
+      <div className=" mx-auto flex flex-col gap-4 my-10 items-center">
         <Header />
+        <SignAndVerify />
       </div>
     </div>
   );
